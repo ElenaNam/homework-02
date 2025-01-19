3f7fe08310322565581808d6b2d227538d6b768b
ElenaNam <zaidelsonelena@gmail.com>
2025-01-20 01:26:27 +0400

    task hw02 and hw03

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -3,30 +3,30 @@ import Affairs from './affairs/Affairs'
 import s2 from '../../s1-main/App.module.css'
 
 /*
-* 1 - описать типы AffairPriorityType, AffairType
-* 2 - указать нужный тип для defaultAffairs
+* 1 - 💥 описать типы AffairPriorityType, AffairType
+* 2 - 💥 указать нужный тип для defaultAffairs
 * 3 - дописать типы и логику функции filterAffairs и проверить её тестами
 * 4 - выполнить пункт 3 для функции deleteAffair
-* 5 - указать нужный тип в useState с affairs
-* 6 - дописать тип и логику функции deleteAffairCallback
-* 7 - в файле Affairs.tsx дописать типизацию пропсов
-* 8 - в файле Affairs.tsx дописать логику функций setAll, setHigh, setMiddle, setLow
-* 9 - в файле Affair.tsx дописать типизацию пропсов
-* 10 - в файле Affair.tsx дописать функции deleteCallback и использовать
-* 11 - в файле Affair.tsx отобразить приходящие данные
+* 5 - 💥 указать нужный тип в useState с affairs
+* 6 - 💥 дописать тип и логику функции deleteAffairCallback
+* 7 - 💥 в файле Affairs.tsx дописать типизацию пропсов
+* 8 - 💥 в файле Affairs.tsx дописать логику функций setAll, setHigh, setMiddle, setLow
+* 9 - 💥 в файле Affair.tsx дописать типизацию пропсов
+* 10 - 💥 в файле Affair.tsx дописать функции deleteCallback и использовать
+* 11 - 💥 в файле Affair.tsx отобразить приходящие данные
 * */
 
 // types
-export type AffairPriorityType = any // need to fix any
+export type AffairPriorityType = 'high' | 'low' | 'middle'
 export type AffairType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: number 
+    name: string 
     priority: AffairPriorityType
 }
 export type FilterType = 'all' | AffairPriorityType
 
 // constants
-const defaultAffairs: any = [ // need to fix any
+const defaultAffairs: AffairType[] = [ 
     {_id: 1, name: 'React', priority: 'high'}, // студенты могут изменить содержимое name и количество элементов в массиве, ...priority не менять!
     {_id: 2, name: 'anime', priority: 'low'},
     {_id: 3, name: 'games', priority: 'low'},
@@ -35,25 +35,25 @@ const defaultAffairs: any = [ // need to fix any
 ]
 
 // pure helper functions
-export const filterAffairs = (affairs: any, filter: any): any => { // need to fix any
+export const filterAffairs = (affairs: AffairType[], filter: FilterType): AffairType[] => { 
 
-
-    return affairs // need to fix
+    const newAffairs = affairs.filter(item => filter !== 'all' ? item.priority === filter : item)
+    return newAffairs
 }
-export const deleteAffair = (affairs: any, _id: any): any => { // need to fix any
+export const deleteAffair = (affairs: AffairType[], _id: number): AffairType[] => { 
 
-    return affairs // need to fix
+    const newAffairs = affairs.filter(item => item._id !== _id)
+    return newAffairs
 }
 
 function HW2() {
-    const [affairs, setAffairs] = useState<any>(defaultAffairs) // need to fix any
+    const [affairs, setAffairs] = useState<AffairType[]>(defaultAffairs) 
     const [filter, setFilter] = useState<FilterType>('all')
 
     const filteredAffairs = filterAffairs(affairs, filter)
-    const deleteAffairCallback = (_id: any) => { // need to fix any
-        // need to fix
+    const deleteAffairCallback = (_id: number) => { 
+        setAffairs(deleteAffair(affairs, _id))
     }
-
     return (
         <div id={'hw2'}>
             <div className={s2.hwTitle}>Homework #2</div>
