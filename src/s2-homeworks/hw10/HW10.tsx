b81c2e123b22022eeb9ccdedd2f73674b3bb0162
ElenaNam <zaidelsonelena@gmail.com>
2025-03-07 11:39:52 +0400

    add hw10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -15,18 +15,20 @@ import {Loader} from './Loader'
 
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    const dispatch = useDispatch();
+
+    const selectLoading = (state: AppStoreType): {isLoading: boolean} => state.loading
+    
+    const isLoading = useSelector(selectLoading).isLoading;
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
-        // dispatch
-
-        // setTimeout
+        dispatch(loadingAC(true))
+        setTimeout(() => {dispatch(loadingAC(false))}, 1500)
     }
 
     return (
         <div id={'hw10'}>
             <div className={s2.hwTitle}>Homework #10</div>
-
             <div className={s2.hw}>
                 {isLoading ? (
                     <div id={'hw10-loading'}>
